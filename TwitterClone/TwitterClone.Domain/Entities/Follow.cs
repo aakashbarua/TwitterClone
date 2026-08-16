933310ce236869e3aaebff35d2554b11a0902670
= <baruaakash343@gmail.com>
2026-08-16 20:37:27 +0600

    Remove code duplication, add override method and apply constructor chaining

--- TwitterClone/TwitterClone.Domain/Entities/Follow.cs
+++ TwitterClone/TwitterClone.Domain/Entities/Follow.cs
@@ -1,34 +1,30 @@
 ﻿
 namespace TwitterClone.Domain.Entities
 {
-    public class Follow
+    public class Follow: BaseEntity
     {
-        private Guid _id; 
+    
         private Guid _followerId;
         private Guid _followeeId;
         private DateTime _followedAt;
-        private DateTime _createdAt;
-        private DateTime _updatedAt;
-        private string _createdBy;
-        private string _updatedBy;
-        public Follow(Guid followerId, Guid followeeId, DateTime followedAt,string createdBy)
+        public Follow()
         {
-            _id = Guid.NewGuid();
-            _createdAt = DateTime.UtcNow;
-            _createdBy = createdBy;
-            _followerId = followerId;
-            _followeeId = followeeId;
-            _followedAt = followedAt;
         }
 
-        public Guid Id{ get { return _id; } }
-        public Guid FollowerId { get { return _followerId; } }
-        public Guid FolloweeId { get { return _followeeId;  } }
-        public DateTime FollowedAt { get { return _followedAt;  } }
-        public DateTime CreatedAt { get { return _createdAt; } }
-        public DateTime UpdatedAt { get { return _updatedAt; } }
+       
+        public Guid FollowerId { 
+            get { return _followerId; }
+            set { _followerId = value; }
+        }
+        public Guid FolloweeId { 
+            get { return _followeeId;  }
+            set { _followeeId = value; }
+        }
+        public DateTime FollowedAt {
+            get { return _followedAt; }
+            set { _followedAt = value; }
 
-        public string CreatedBy { get { return _createdBy; } }
-        public string UpdatedBy { get { return _updatedBy; } }
+        }
+       
     }
 }
