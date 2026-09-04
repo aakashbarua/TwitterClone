a95862e893ca945242f6ec3de2da601bece93cfa
= <baruaakash343@gmail.com>
2026-09-05 00:41:39 +0600

    Polymorphism & Interface

--- TwitterClone/TwitterClone.Domain/Entities/User.cs
+++ TwitterClone/TwitterClone.Domain/Entities/User.cs
@@ -1,6 +1,6 @@
 ﻿namespace TwitterClone.Domain.Entities
 {
-    public class User : BaseEntity
+    public class User : BaseEntity,IFollowable,INotifiable
     {
         private string _firstName;
         private string _lastName;
@@ -11,5 +11,40 @@
         public string FirstName { get { return _firstName; } set { _firstName = value; } }
         public string LastName { get { return _lastName; } set { _lastName = value; } }
         public string Email { get { return _email; } set { _email = value; } }
+
+        private List<Guid> _followers= new List<Guid>();
+        private List<Guid> _incomingnotifications= new List<Guid>();    
+
+        public void Follow(Guid userId)
+        {
+            if (!_followers.Contains(userId))
+            { 
+                _followers.Add(userId);
+            }
+
+        }
+
+        public void Unfollow(Guid userId) 
+        {
+            if (_followers.Contains(userId))
+            {
+                _followers.Remove(userId);
+            }
+
+        }
+
+        public override string DescribeRecord()
+        {
+            var baseRecord = base.DescribeRecord();
+            return $"{baseRecord}, FirstName: {FirstName}, LastName: {LastName}, Email: {Email}";
+        }
+
+        public void AddNotification(Guid notificationId)
+        {
+            if (!_incomingnotifications.Contains(notificationId))
+            {
+                _incomingnotifications.Add(notificationId); 
+            }
+        }
     }
 }
---
+++ TwitterClone/twitter.test/Abstrction.cs
@@ -0,0 +1,27 @@
+﻿using TwitterClone.Domain.Entities;
+
+public class Abstrction
+{
+    public void Run()
+    {
+        var notifications = new List<Notification>()
+        {
+            new LikeNotification(Guid.NewGuid()),
+            new CommentNotification(Guid.NewGuid()),
+            new FriendRequestNotification(Guid.NewGuid()),
+            new MentionNotification(Guid.NewGuid()),
+            new SystemNotification()
+        };
+
+        foreach (var notification in notifications)
+        {
+            Console.WriteLine(notification.GetMessage());
+        }
+    }
+
+    public static void Main()
+    {
+        var abstraction = new Abstrction();
+        abstraction.Run();
+    }
+}
